6b01f20f8affbcbba1e96792381a3451f7920094
GuillenA7 <a22310361@ceti.mx>
2025-10-15 17:23:01 -0600

    feat: initialize client data and display account information in console

---
+++ src/Desafio.java
@@ -0,0 +1,12 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nombre = "Tony Stark";
+        String tipoDeCuenta = "Corriente";
+        double saldo = 1599.99;
+
+        System.out.println("******************");
+        System.out.println("\nNombre del cliente: " + nombre);
+        System.out.println("Tipo de cuenta: " + tipoDeCuenta);
+        System.out.println("Saldo disponible: " + saldo + "$");
+    }
+}
